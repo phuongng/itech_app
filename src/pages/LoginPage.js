5628141bb34effeec0698982c55742a2a20275df
Phuong Nguyen <78459468+phuongng@users.noreply.github.com>
2023-11-27 18:31:00 -0500

    update login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -44,64 +44,66 @@ return (
                 <img src={logo} className="loginImageLeft" />
                 <h1>Log in</h1>
                 <p>See your growth and get support</p>
-                    <div className="form">
+                    
                         <form onSubmit={loginUser}>
+                            
                             <div className="form">
                                 <label>
                                     Username:
                                     <input type="text" name="username" placeholder="Enter username"/>
-                                    <div className="form">
-                                    Password:
+                                </label>
+                            </div>        
+                            
+                            <div className="form">
+                                <label>Password:  
                                     <div className="password-input">
                                     <input type={showPassword ? "text" : "password"} name="password" placeholder="Enter password"/>
                                     <button type="button" onClick={toggleShowPassword} className="showPasswordButton">
                                         {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
                                     </button>
                                     </div>
-                                    <div className="beforeLogin">
-                                        <div className="rememberMe">
-                                        <Checkbox
-                                            className="checkBox"
-                                            checked={checked}
-                                            onChange={handleChange}
-                                            inputProps={{ "aria-label": "controlled" }}
-                                        />
-                                        <p>Remember me</p>
-                                        </div>
-                                        <div>
-                                        <p>Forgot Password</p>
-                                        </div>
-                                    </div>
-                                    <button className="loginButton" >
-                                        Login
-                                    </button>
-                                    </div>     
-                                </label>
-                            </div>  
+                                    </label>
+                            </div>
 
-
-
-
-                        </form>
-                    </div>
-            </div>
-
+                            <div className="beforeLogin">
+                                <div className="rememberMe">
+                                <Checkbox
+                                    className="checkBox"
+                                    checked={checked}
+                                    onChange={handleChange}
+                                    inputProps={{ "aria-label": "controlled" }}
+                                />
+                                <p>Remember me</p>
+                                </div>
+                                <div>
+                                <p>Forgot Password</p>
+                                </div>
+                            </div>
+                            
+                            <button className="loginButton" >
+                                Login
+                            </button>
+                            </form>
+                                      
+                                
+            </div>  {/* end login left */}
+                        
+                    
+         
+ {/* login right section */}
                  <div className="loginRight">
                     <img src={logo} className="loginImage" />
                     <p className="introduction">
                     A user-friendly web application meticulously crafted for small
-            businesses, providing a seamless platform to efficiently manage
-            their inventory systems. Empowering businesses with intuitive
-            features for streamlined inventory tracking, this application ensures
-            precision and ease in managing stock, enhancing overall operational
-            efficiency
+                    businesses, providing a seamless platform to efficiently manage
+                    their inventory systems. Empowering businesses with intuitive
+                    features for streamlined inventory tracking, this application ensures
+                    precision and ease in managing stock, enhancing overall operational
+                    efficiency.
                     </p>
                 </div>
-        
-        
-        
-        
-        </div>
+
+        </div> 
     </div>
 
         
